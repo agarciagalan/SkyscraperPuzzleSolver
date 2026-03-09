4e55ef308f3c984e55f541644517ed3c9df5b70a
Alejandro García Galán <alex.garciagalan@hotmail.com>
2026-03-09 15:53:30 +0000

    feat: aditional checks and aux function to manage errors, fixed leaks

--- main.c
+++ main.c
@@ -14,10 +14,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int		*create_board(int *board, int size);
+int		*create_board(int *board, int *combination, int size);
 int		get_combination(char *arg, int *combination, int size);
 void	set_initial_values(int *board, int *combination, int size);
 int		solve(int *board, int size, int position, int *combination);
+
+int	ft_error(void)
+{
+	write(1, "Error\n", 6);
+    return (1);
+}
 
 int	main(int argc, char **argv)
 {
@@ -26,22 +32,22 @@ int	main(int argc, char **argv)
 	int		*combination;
 
 	if (argc != 2)
-	{
-		write(1, "Error\n", 6);
-		return (1);
-	}
-	size = 7;
-	combination = (int *)malloc(size * 4 * sizeof(int));
-	get_combination(argv[1], combination, size);
-	board = (int *)malloc(size * size * sizeof(int));
-	create_board(board, size);
-	set_initial_values(board, combination, size);
-	if (solve(board, size, 0, combination))
-	{
-		write(1, "Error\n", 6);
-		return (1);
-	}
-	free(board);
-	free(combination);
-	return (0);
+        return (ft_error());
+	size = 4;
+    combination = (int *)malloc(size * 4 * sizeof(int));
+    if (!combination || get_combination(argv[1], combination, size))
+        return (free(combination), ft_error());
+    board = (int *)malloc(size * size * sizeof(int));
+    if (!board)
+        return (free(combination), ft_error());
+    create_board(board, combination, size);
+    if (solve(board, size, 0, combination))
+    {
+        free(board);
+        free(combination);
+        return (ft_error());
+    }
+    free(board);
+    free(combination);
+    return (0);
 }
